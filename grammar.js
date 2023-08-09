6109fa4a419243ab37140657e28c283e1db4bd20
Andrea Aquino <andryak@fb.com>
2023-08-09 05:04:21 -0700

    Add support for expression trees

--- grammar.js
+++ grammar.js
@@ -180,6 +180,7 @@ const rules = {
       $.collection,
       $._literal,
       $._variablish,
+      $.expression_tree,
       $.prefixed_string,
       $.parenthesized_expression,
       $.binary_expression,
@@ -406,6 +407,11 @@ const rules = {
         /"(\\"|\\\\|\\?[^"\\])*"/,
       ),
     ),
+
+  expression_tree: $ => seq(
+    field('visitor', $.identifier),
+    token(/`[^`]*`/),
+  ),
 
   prefixed_string: $ => seq(field('prefix', $.identifier), $.string),
 
